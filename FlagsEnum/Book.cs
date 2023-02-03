aa77f3b4e9cc7cfec66c459f86458789edbac391
Jasper Kent <software@jasperkent.com>
2023-02-03 08:35:10 +0000

    Add project files.

---
+++ FlagsEnum/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace FlagsEnum
+{
+    [Flags]
+    public enum Formats { None = 0, Hardback = 1, Paperback = 2, Ebook = 4, AudioBook = 8 }
+
+    public class Book
+    {
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public Formats AvailableFormats { get; set; }
+
+        public override string ToString() => $"{Title} by {Author} ({AvailableFormats})";
+    }
+}
---
+++ FlagsEnum/Program.cs
@@ -0,0 +1,13 @@
+﻿using FlagsEnum;
+
+var book = new Book 
+{ 
+    Title = "Twelve", 
+    Author = "Jasper Kent",
+    AvailableFormats = Formats.Hardback | Formats.AudioBook | Formats.Paperback
+};
+
+Console.WriteLine(book);
+
+if (book.AvailableFormats.HasFlag (Formats.AudioBook))
+    Console.WriteLine("It's available as an audio book.");
